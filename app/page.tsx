147094359da8c8bb153983dd3aa57b8d9c7d8aca
Adil Ashurov <ashurovadil96@gmail.com>
2024-07-03 20:00:37 +0400

    fix code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials";
 import ModalProductBag from "@/components/ModalProductBag";
 
 export default function Home() {
